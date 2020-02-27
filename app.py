0993b9d2d5b41225e9c56357405c4f5bebd5a06e
pcbcos <oxygen10266@gmail.com>
2020-02-27 22:12:55 +0800

    2

---
+++ app.py
@@ -0,0 +1,9 @@
+import flask
+import subprocess
+import time
+app = flask.Flask(__name__)
+@app.route('/')
+def todownload():
+    localtime = time.asctime( time.localtime(time.time()) )
+    with open("test.txt","w") as f:
+        f.writelines(localtime)

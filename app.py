7f87e7e752cbedd50f5b27504ee578ebdda63697
pcbcos <oxygen10266@gmail.com>
2020-03-01 12:13:56 +0800

    6

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import flask
-import subprocess
+from os import system
 import time
 app = flask.Flask(__name__)
 @app.route('/',methods=["POST"])
 def todownload():
     if flask.request.method=="POST":
+        system("python3 test.py")
         localtime = time.asctime( time.localtime(time.time()) )
         with open("test.txt","w") as f:
             f.writelines(localtime)
